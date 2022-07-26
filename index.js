8e240f0f18a2b81472f5c2da8b95d263dfdf720d
Dilli1998 <109887297+Dilli1998@users.noreply.github.com>
2022-07-26 15:34:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// To compare Two JSON have the same properties without order:-
+
+// var obj1= {name:"person1",age:5}
+// var obj2= {age:5,name:"person1"}
+// console.log(_.isEqual(obj1, obj2));
+
+//Display all the country in the console
+
+// let request = new XMLHttpRequest()
+
+// request.open('GET','https://62df6675976ae7460bebae34.mockapi.io/Country')
+// request.send()
+
+// request.onreadystatechange = function(){
+//      let response = JSON.parse(this.response)
+//      response.forEach(element => {
+//         console.log(element.Flag);
+//      });
+// }
+
+//Display Country,Region,Subregion,Population
+
+// let request = new XMLHttpRequest()
+
+// request.open('GET','https://62df6675976ae7460bebae34.mockapi.io/Country')
+// request.send()
+
+// request.onreadystatechange = function(){
+//      let response = JSON.parse(this.response)
+//      response.forEach(element => {
+//         console.log(element.country,element.Region,element.Subregion,element.Population);
+//      });
+// }
+
